4e14663b34bbdafa90a06d35e9bbd8e13bcd5fe8
CAPSTONEID <sinsra2@gmail.com>
2026-03-23 13:58:08 +0900

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@
   import path from 'path';
 
   export default defineConfig({
+    base: './',
     plugins: [react(), tailwindcss()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
